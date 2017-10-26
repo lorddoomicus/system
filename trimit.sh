97838fcaba47b232ffa6d4180641f2b8987bd111
Derrik Walker v2.0 <dwalker@doomd.net>
2017-10-26 18:15:14 -0400

    trimit.sh	- fixed issue with date in logs

--- trimit.sh
+++ trimit.sh
@@ -3,13 +3,14 @@
 # Runs fstrim with some logging - this should be put in /etc/cront.weekly
 #
 # 2017-10-05	dw2	Initial Version
+# 2017-10-26	dw2	Fixed Date
 #
 
 logfile=/var/log/fstrim.log
 
 logger running fstrim ... 
 
-$date			>> $logfile 
+$(date)			>> $logfile
 /usr/sbin/fstrim -a -v	>> $logfile
 echo			>> $logfile
 
