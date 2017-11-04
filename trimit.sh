34c21f360bf80251ad04d0434401dab7d7dd53be
Derrik Walker v2.0 <dwalker@doomd.net>
2017-11-04 19:07:35 -0400

    trimit.sh - fixed date ... hopefully for real this time

--- trimit.sh
+++ trimit.sh
@@ -4,13 +4,13 @@
 #
 # 2017-10-05	dw2	Initial Version
 # 2017-10-26	dw2	Fixed Date
-#
+# 2017-11-04	dw2	Still fixing Date
 
 logfile=/var/log/fstrim.log
 
 logger running fstrim ... 
 
-$(date)			>> $logfile
+date			>> $logfile
 /usr/sbin/fstrim -a -v	>> $logfile
 echo			>> $logfile
 
